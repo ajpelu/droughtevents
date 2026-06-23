32606f337cee6df1ce1132d4484423d35f935043
ajpelu <ajpelu@gmail.com>
2026-06-23 19:09:22 +0200

    change asserthat by cli::cli

--- R/droughts.R
+++ R/droughts.R
@@ -10,10 +10,10 @@
 #' - a `date` column of class `Date` or `POSIXct`, or
 #' - separate `year` and `month` columns.
 #' - a numeric column specified by `vname` that contains the drought index values.
-#'
 #' @param vname A string indicating the name of the numeric column to be evaluated (e.g., `"spei12"`).
-#'
 #' @param threshold A numeric threshold below which values are considered part of a drought event.
+#' @param min_duration Minimum number of consecutive months below `threshold` required for an
+#' event to be considered a drought. Default is 2.
 #'
 #' @return A named list with three elements:
 #' * `data`: the original data frame with drought flags and durations.
@@ -25,37 +25,34 @@
 #' # Detect droughts in the SPEI-12 time series with a threshold of -1.28
 #' droughts_result <- droughts(spei, vname = "spei12", threshold = -1.28)
 #'
-#' @importFrom assertthat assert_that
-#' @importFrom lubridate make_date month year
+#' @importFrom lubridate make_date
 #' @importFrom dplyr mutate group_by summarise filter ungroup
 #' @importFrom data.table rleid
 #' @export
 
-droughts <- function(df, vname, threshold) {
+droughts <- function(df, vname, threshold, min_duration = 2) {
 
-  assertthat::assert_that(
-    inherits(df, "data.frame"),
-    msg = "The input must be a data frame or tibble."
-  )
+  if (!inherits(df, "data.frame")) {
+    cli::cli_abort("{.arg df} must be a data frame or tibble.")
+  }
 
-  assertthat::assert_that(
-    vname %in% names(df),
-    msg = paste("Variable", vname, "not found in the data frame.")
-  )
+  if (!vname %in% names(df)) {
+    cli::cli_abort("Variable {.val {vname}} not found in the data frame.")
+  }
 
-  assertthat::assert_that(
-    is.numeric(df[[vname]]),
-    msg = paste("Variable", vname, "must be numeric.")
-  )
+  if (!is.numeric(df[[vname]])) {
+    cli::cli_abort("Variable {.val {vname}} must be numeric.")
+    }
 
-  assertthat::assert_that(
-    is.numeric(threshold),
-    msg = "Threshold must be a numeric value."
-  )
+  if (!is.numeric(threshold)) {
+    cli::cli_abort("Threshold must be a numeric value.")
+  }
+
+  if (!is.numeric(min_duration)) {
+    cli::cli_abort("{.arg {min_duration}} must be a numeric value.")
+  }
 
   df <- .check_date(df)
-
-  min_duration <- 2 # Default minimum duration for drought events
 
   aux <- df |>
     dplyr::mutate(

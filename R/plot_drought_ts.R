453efbdc0287f95b15fb1dcf2b2848f300343775
ajpelu <ajpelu@gmail.com>
2026-06-24 18:48:21 +0200

    update doc

--- R/plot_drought_ts.R
+++ R/plot_drought_ts.R
@@ -8,24 +8,14 @@
 #' @param df A `data.frame` or `tibble` containing the time series data.
 #'
 #' @param vname A string. Name of the numeric column representing the drought index.
-#'
 #' @param title An optional character string for the plot title.
-#'
 #' @param date_col A string. Name of the column containing date information (default is `"date"`).
-#'
-#' @param pos_col Color used for positive values (default is `"blue"`).
-#'
-#' @param neg_col Color used for negative values (default is `"red"`).
-#'
-#' @param zero_line_col Color of the horizontal line at zero (default is `"black"`).
-#'
+#' @param pos_color Color used for positive values (default is `"blue"`).
+#' @param neg_color Color used for negative values (default is `"red"`).
+#' @param zero_line_color Color of the horizontal line at zero (default is `"black"`).
 #' @param zero_line_linetype Line type for the horizontal zero line (default is `"solid"`).
-#'
 #' @param by_year Logical. If `TRUE`, aggregates the drought index by year and plots annual means (default is `FALSE`).
-#'
 #' @param y_axis_title Optional label for the y-axis. If `NULL`, the name of `vname` is used.
-#'
-#' @param ... Additional arguments passed to `ggplot2::ggplot()`.
 #'
 #' @return A `ggplot` object representing the drought index time series as a bar plot.
 #'
@@ -41,8 +31,8 @@
 #'
 #' @seealso [droughts()]
 #'
-#' @importFrom assertthat assert_that
 #' @importFrom lubridate year make_date
+#' @importFrom rlang .data
 #' @importFrom dplyr mutate filter group_by summarise
 #' @importFrom ggplot2 ggplot aes geom_col geom_hline labs scale_fill_manual theme_bw theme element_rect element_text
 #' @export
@@ -51,9 +41,9 @@ plot_drought_ts <- function(df,
                       vname,
                       title = NULL,
                       date_col = "date",
-                      pos_col = "blue",
-                      neg_col = "red",
-                      zero_line_col = "black",
+                      pos_color = "blue",
+                      neg_color = "red",
+                      zero_line_color = "black",
                       zero_line_linetype = "solid",
                       by_year = FALSE,
                       y_axis_title = NULL) {
@@ -68,15 +58,22 @@ plot_drought_ts <- function(df,
     )
   }
 
-  assertthat::assert_that(
-    is.numeric(df[[vname]]),
-    msg = paste("Variable", vname, "must be numeric.")
-  )
+  if (!is.numeric(df[[vname]])) {
+    cli::cli_abort("Variable {.val {vname}} must be numeric.")
+  }
 
-  assertthat::assert_that(
-    date_col %in% names(df),
-    msg = paste("Variable", date_col, "not found in the data frame.")
-  )
+  if (!date_col %in% names(df)) {
+    cli::cli_abort(
+      "Column {.val {date_col}} is not present in {.arg df}."
+    )
+  }
+
+  if (!inherits(df[[date_col]], c("Date", "POSIXct"))) {
+    cli::cli_abort(
+      "Column {.val {date_col}} must be of class {.cls Date} or {.cls POSIXct}."
+    )
+  }
+
 
   if (is.null(y_axis_title)) {
     y_axis_title <- vname
@@ -88,16 +85,16 @@ plot_drought_ts <- function(df,
   if (by_year) {
     df <- df |>
       dplyr::mutate(year = lubridate::year(.data[[date_col]])) |>
-      dplyr::group_by(year) |>
+      dplyr::group_by(.data$year) |>
       dplyr::summarise(
         value = mean(.data[[vname]], na.rm = TRUE),
         .groups = "drop"
       ) |>
-      dplyr::mutate(date = lubridate::make_date(year = year, month = 1, day = 1))
+      dplyr::mutate(plot_date = lubridate::make_date(year = .data$year, month = 1, day = 1))
 
-    vname <- "value"  # renombra para usar en el plot
-    date_col <- "date"
-    title <- paste(title, "(groupped by year)")
+    vname <- "value"  # Rename to use the plot
+    date_col <- "plot_date"
+    title <- if (is.null(title)) "(grouped by year)" else paste(title, "(grouped by year)")
   }
 
   # compute sign
@@ -106,8 +103,8 @@ plot_drought_ts <- function(df,
 
   p <- ggplot2::ggplot(df, ggplot2::aes(x = .data[[date_col]], y = .data[[vname]], fill = sign)) +
     ggplot2::geom_col(color = NA) +
-    ggplot2::scale_fill_manual(values = c(pos = pos_col, neg = neg_col)) +
-    ggplot2::geom_hline(yintercept = 0, linetype = zero_line_linetype, color = zero_line_col) +
+    ggplot2::scale_fill_manual(values = c(pos = pos_color, neg = neg_color)) +
+    ggplot2::geom_hline(yintercept = 0, linetype = zero_line_linetype, color = zero_line_color) +
     ggplot2::labs(
       title = title,
       x = NULL,
